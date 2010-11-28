17fceba83bf44b7adbc2b3a9f2ad5543bbd59c47
Noah <sungs@comcast.net>
2010-11-28 02:19:50 -0800

    First test Facebook app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstGame::Application.config.secret_token = '0a25f3537c56ff579e8247f0bb9ef2c7454fa220f64d4296f5e75b92d6a5f3342955d95dd7e36f1d095557ac0841ae8cbae667d151ef3ebe3de59462bd7c0110'
